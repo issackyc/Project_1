fb73c61f7f16e907fd3bd468352db29df9c57e30
issackwok <issackwok@10.211.55.3>
2024-03-29 22:49:01 +0800

    First Commit

---
+++ Project_java_1/src/test_1.java
@@ -0,0 +1,8 @@
+
+public class test_1 {
+	   public static void main(String[] args) {
+	       String version="1.0"; 
+		   System.out.println("Hello, World! version:"+version); 
+	        
+	   }
+}
