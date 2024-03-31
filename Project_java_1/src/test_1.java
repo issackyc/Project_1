f66ce0d4e266d1cd9a3e425753f9b7851d55d0e5
issackyc <38339166+issackyc@users.noreply.github.com>
2024-03-31 11:52:04 +0800

    version 2.0 commit

--- Project_java_1/src/test_1.java
+++ Project_java_1/src/test_1.java
@@ -1,7 +1,7 @@
 
 public class test_1 {
 	   public static void main(String[] args) {
-	       String version="1.0"; 
+	       String version="2.0"; 
 		   System.out.println("Hello, World! version:"+version); 
 	        
 	   }
