7f880191586c23d72e411e9bff19e228543c22a4
issackyc <38339166+issackyc@users.noreply.github.com>
2024-04-07 08:55:00 +0800

    commit 6.0

--- Project_java_1/src/test_1.java
+++ Project_java_1/src/test_1.java
@@ -1,7 +1,7 @@
 
 public class test_1 {
 	   public static void main(String[] args) {
-	       String version="3.0"; 
+	       String version="6.0"; 
 		   System.out.println("Hello, World! version:"+version); 
 		   System.out.println("feature_1"); 
 	        
